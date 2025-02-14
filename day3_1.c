96c61d6ae8a16dd9090243392e0cdb1545f6ad88
saphiul <148545272+saphiul@users.noreply.github.com>
2025-02-14 10:12:15 +0530

    Add files via upload
    day3_1.c data types and size of data types.

---
+++ day3_1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+int main(void)
+{
+    int val=40; //val is a variable of type int
+    ///***val is holding value 40
+    //variable initialization
+    int num;// variable declaration
+
+    int n1=50,n2=30;
+    num=10; //variable definition
+
+    printf("Value  = %d",val);
+    printf("Number =  %d",num);
+
+    printf("\n  N1 = %d  N2 = %d ",n1,n2);
+    printf("n1 = %u and n2 = %u\n",&n1,&n2);
+   
+
+    return 0;
+}*/
+int main(void){
+int myNum = 15;            // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'C';       // Character
+
+// Print variables
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter);
+printf("My favorite number is: %d\n", 15);
+printf("My favorite letter is: %c", 'D');
+return 0;}
